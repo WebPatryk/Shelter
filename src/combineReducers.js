0879875c81d5949afc04b00cf59138283014c42e
Patryk Lekki <plekki@cm.local>
2021-02-26 13:15:00 +0100

    adding redux

---
+++ src/combineReducers.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import loginReducer from './redux/user/reducer';
+
+export default combineReducers({
+  loginReducer,
+});
--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import combineReducers from './combineReducers';
 import Root from './view/Root/Root';
 import reportWebVitals from './reportWebVitals';
 
+const store = createStore(
+  combineReducers,
+  // eslint-disable-next-line no-underscore-dangle
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
 ReactDOM.render(
   <React.StrictMode>
-    <Root />
+    <Provider store={store}>
+      <Root />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
